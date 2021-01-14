8865014376d466577981d07d2d708c0d9321fe7a
Fernando Vintacourt <vintacourtfernando@gmail.com>
2021-01-14 16:54:56 -0300

    Delete tuples.py

--- python/tuples.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    integer_list = map(int, input().split())
-    
-    t = []
-    for i in integer_list:
-        t.append(i)
-        
-    print(hash(tuple(t)))
