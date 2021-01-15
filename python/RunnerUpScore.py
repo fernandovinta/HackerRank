b0e9013ec628881406fcd7e5a5922f6842c1e384
Fernando Vintacourt <vintacourtfernando@gmail.com>
2021-01-15 00:36:08 -0300

    Update RunnerUpScore.py

--- python/RunnerUpScore.py
+++ python/RunnerUpScore.py
@@ -1,10 +1,5 @@
 if __name__ == '__main__':
     n = int(input())
-    arr = map(int, input().split())
-    champion = runnerup = -100
-    for i in arr:
-        if i > champion:
-            champion = i
-        if i >= runnerup and i < champion:
-            runnerup = i
-    print(runnerup) 
+    arr = list(map(int, input().split()))
+    a = sorted(list(set(arr)))
+    print(a[len(a)-2])
